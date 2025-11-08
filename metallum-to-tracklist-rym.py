2cbfe50c07d317f12380296b9d45ca95aba09e12
Nadia <87604069+nadiakay@users.noreply.github.com>
2025-11-08 03:05:57 -0600

    Update metallum-to-tracklist-rym.py
    typo

--- metallum-to-tracklist-rym.py
+++ metallum-to-tracklist-rym.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 		out = ""
 		soup = BeautifulSoup(r, 'html.parser')
 		for each in soup.find(class_="table_lyrics").tbody.find_all(class_=['even', 'odd']):
-			out += each.findChild().contents[1][:1] + "|" + each.find(class_='wrapWords').contents[0].strip() + "|" + out += each.find(attrs={'align': 'right'}).contents[0] + '\n'
+			out += each.findChild().contents[1][:1] + "|" + each.find(class_='wrapWords').contents[0].strip() + "|" + out + each.find(attrs={'align': 'right'}).contents[0] + '\n'
 		title = soup.title.contents[0].rsplit('-',1)[0]
 		with open(f'out/{title}.txt', "w", encoding="utf-8") as f:
 			f.write(out)
